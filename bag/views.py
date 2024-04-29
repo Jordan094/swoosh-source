aadf774f8e70f63849526e252edcb808b460a1a2
Jordan Cooper <jordan_cooper@outlook.com>
2024-04-29 21:31:29 +0000

    removed size functionality until working correctly

--- bag/views.py
+++ bag/views.py
@@ -8,25 +8,16 @@ def view_bag(request):
 
 
 def add_to_bag(request, item_id):
-    """This adds a quantity of the specified item with selected size to the bag"""
+    """This adds a quantity of the specified item to the bag """
 
-    quantity = int(request.POST.get('quantity', 1))
-    size = request.POST.get('size')
+    quantity = (request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
-    if item_id in bag.keys():
-        if isinstance(bag[item_id], dict) and 'items_by_size' in bag[item_id]:
-            if size in bag[item_id]['items_by_size'].keys():
-                bag[item_id]['items_by_size'][size] += quantity
-            else:
-                bag[item_id]['items_by_size'][size] = quantity
-        else:
-            # Initialize bag[item_id] as a dictionary with items_by_size
-            bag[item_id] = {'items_by_size': {size: quantity}}
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
     else:
-        # Initialize bag[item_id] as a dictionary with items_by_size
-        bag[item_id] = {'items_by_size': {size: quantity}}
+        bag[item_id] = quantity
 
     request.session['bag'] = bag
     print(request.session['bag'])
