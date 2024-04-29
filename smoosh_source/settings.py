761e80b4aab6c02f1e7e0fa371072c58097c4c08
Jordan Cooper <jordan_cooper@outlook.com>
2024-04-29 19:12:00 +0000

    Added bag context and delivery percentage

--- smoosh_source/settings.py
+++ smoosh_source/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'products',
+    'bag',
 
 
 ]
@@ -77,6 +78,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # needed by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -160,6 +162,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STANDARD_DELIVERY_PERCENTAGE = 5
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
