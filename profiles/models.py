9f65dc4642990b2fb09f39c74efce7300e0d744e
Jordan Cooper <jordan_cooper@outlook.com>
2024-05-19 08:51:40 +0000

    Updated allauth templates

--- profiles/models.py
+++ profiles/models.py
@@ -8,7 +8,7 @@ from django_countries.fields import CountryField
 
 class UserProfile(models.Model):
     """
-    A user profile model to hold delivery info
+    A user profile model to hold delivery
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
