737411e4ff415fd1b486924af6148d14beafd642
Jordan Cooper <jordan_cooper@outlook.com>
2024-05-26 10:48:00 +0000

    Finished add product functionality

--- profiles/views.py
+++ profiles/views.py
@@ -16,9 +16,11 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
-    orders = profile.Orders.all()
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
@@ -28,6 +30,7 @@ def profile(request):
     }
 
     return render(request, template, context)
+
 
     
 def order_history(request, order_number):
