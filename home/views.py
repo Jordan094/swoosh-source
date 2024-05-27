30883e30749800f1d2cd31c8172668ae757249a2
Jordan Cooper <jordan_cooper@outlook.com>
2024-05-27 18:21:11 +0000

    Added footer and homepage styling

--- home/views.py
+++ home/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
-
+from products.models import Product  # Adjust this import statement based on your actual app structure
 
 def index(request):
-    """A view to return the index page """
-    
-    return render(request, 'home/index.html')
+    """A view to return the index page with random products"""
+    # Retrieve three random products
+    random_products = Product.objects.order_by('?')[:3]
+    context = {
+        'random_products': random_products
+    }
+    return render(request, 'home/index.html', context)
