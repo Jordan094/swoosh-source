e7b47ffe7ad66245677f4a4d628cd57fdc3ca46b
Jordan Cooper <jordan_cooper@outlook.com>
2024-05-30 18:53:32 +0000

    Deployment

--- swoosh_source/settings.py
+++ swoosh_source/settings.py
@@ -31,7 +31,6 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ['swoosh-source-caf22a2ccbfa.herokuapp.com', 'localhost']
-ALLOWED_HOSTS = ['8000-jordan094-swooshsource-bx4437k0948.ws-eu114.gitpod.io', 'localhost']
 
 
 
@@ -53,10 +52,10 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
+    'storages',
 
 
     'crispy_forms',
-    'storages',
 
 
 ]
@@ -186,21 +185,22 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'swoosh-source'
-    AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_STORAGE_BUCKET_NAME = 'ckz8780-boutique-ado'
+    AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-    #Static & Media
+    # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
-    # Override static and media URLs
+    # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+    
 
 
 # Default primary key field type
