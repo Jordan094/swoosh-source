f77ec84fa751e8cffda02045848dce2b4d08e28c
Jordan Cooper <jordan_cooper@outlook.com>
2024-05-30 19:51:04 +0000

    Deployed

--- swoosh_source/settings.py
+++ swoosh_source/settings.py
@@ -216,3 +216,6 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
