272063421007c7eb32be8c15562d25849e9a754a
Jordan Cooper <jordan_cooper@outlook.com>
2024-06-01 10:40:43 +0000

    Added favourites buttons and css fixes

--- swoosh_source/settings.py
+++ swoosh_source/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['swoosh-source-caf22a2ccbfa.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['swoosh-source-caf22a2ccbfa.herokuapp.com', 'localhost']
 
 
 
